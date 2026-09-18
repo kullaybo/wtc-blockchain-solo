713a935cdb5672ba2e35dc5ecc1ccec2b0f370c0
Emanuel Serake <emserjhb025@student.wethinkcode.co.za>
2026-09-18 13:55:10 +0200

    feat: update Hardhat configuration and add dotenv dependency

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
-import type { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: "0.8.34", 
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
 };
 
 export default config;
